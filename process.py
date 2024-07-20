587613f8f452bed2abe9c8acfa21f4c3d880577e
Tim Wu <timwu@ipevo.com>
2024-07-20 21:35:36 +0800

    more experiments to combine paragraphs

--- process.py
+++ process.py
@@ -2,14 +2,15 @@ import docx
 import sys
 from simplify_docx import simplify
 
-
 def parse_paragraph( parav):
-    if len(parav)==1:
-        parav0 = parav[0]['VALUE']
-        print(parav0)
+    assert len(parav)==1, "length of 'paragraph' is not 1"
+    parav0 = parav[0]['VALUE']
+    print(parav0)
 
 def parse_table( tablev):
-    print( len(tablev))
+    assert len(tablev)==1, "length of 'table' is not 1"
+    tablev0=tablev[0] 
+    print(tablev0)
 
 
 def tryparse( dd):
@@ -19,16 +20,19 @@ def tryparse( dd):
     dv0 = dv[0]
     assert dv0['TYPE']=='body', "the first element of 'document' is not a 'body'"
     bodyv = dv0['VALUE']
-    print(f'{len(bodyv)} paragraphs')
+    print(f'{len(bodyv)} blocks')
     #para0 = bodyv[0]
-    for blk in bodyv:
+    for idx, blk in enumerate(bodyv):
         #assert para['TYPE']=='paragraph'
-        if blk['TYPE']=='paragraph':
-            parse_paragraph(blk['VALUE'])
-        elif blk['TYPE']=='table':
-            parse_table(blk['VALUE'])
+        blkt = blk['TYPE']
+        blkv = blk['VALUE']
+        print(f'block {idx} type {blkt}')
+        if blkt=='paragraph':
+            parse_paragraph( blkv)
+        elif blkt=='table':
+            parse_table(blkv)
         else:
-            print(f"Unknown type {blk['TYPE']}")
+            print(f"Unknown type {blkt}")
 
     #    print(
 # read in a document 
