3d1718177513ec39155fd50049d446fb0f72c563
Tim Wu <timwu@ipevo.com>
2024-12-28 07:41:59 +0800

    chore: comment out alternative model configurations in settings for clarity

--- testftm_ui.py
+++ testftm_ui.py
@@ -17,6 +17,7 @@ cl.instrument_openai()
 
 settings = {
     "model": 'ft:gpt-4o-2024-08-06:ipevo-corp:20241226:Aia7iefG',
+    #"model": 'ft:gpt-4o-2024-08-06:ipevo-corp:fgs-glossary-2024dec:AiKlT6c7',
     #"model": "ft:gpt-4o-mini-2024-07-18:ipevo-corp::9qI6ek8y",
     #"model": 'ft:gpt-4o-mini-2024-07-18:ipevo-corp:fgs-glossary3:9ssy45aS',
     #"model": 'ft:gpt-4o-mini-2024-07-18:ipevo-corp:fgs-glossary3:9srFRIKs',
